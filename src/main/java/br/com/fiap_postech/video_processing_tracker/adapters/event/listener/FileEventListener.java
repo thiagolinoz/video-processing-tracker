6253b9bd1b736ca1aca7453a613f7e0e322b1d82
thiagolinoz <thiagolinoz@gmail.com>
2026-02-19 22:00:13 -0300

    Adiciona Kafka configs

--- src/main/java/br/com/fiap_postech/video_processing_tracker/adapters/event/listener/FileEventListener.java
+++ src/main/java/br/com/fiap_postech/video_processing_tracker/adapters/event/listener/FileEventListener.java
@@ -6,17 +6,44 @@ import br.com.fiap_postech.video_processing_tracker.adapters.dto.VideoUploadedCl
 import br.com.fiap_postech.video_processing_tracker.domain.models.VideoModel;
 import br.com.fiap_postech.video_processing_tracker.domain.models.events.VideoUploadedModel;
 import br.com.fiap_postech.video_processing_tracker.domain.ports.in.VideoMetadataServicePort;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class FileEventListener {
 
     private final VideoMetadataServicePort videoMetadataServicePort;
+    private final ObjectMapper objectMapper  = new ObjectMapper();
+    private static final Logger logger = Logger.getLogger(FileEventListener.class.getName());
 
     public FileEventListener(VideoMetadataServicePort videoMetadataServicePort) {
         this.videoMetadataServicePort = videoMetadataServicePort;
     }
 
-    public void consume(VideoUploadedCloudEvent event) {
-        VideoUploadedModel videoUploadedModel = CloudEventMapper.toModel(event);
-        videoMetadataServicePort.handleVideo(VideoMapper.toVideoModel(videoUploadedModel));
+    @KafkaListener(
+            topics = "received-videos",
+            groupId = "${kafka.consumer.group-id}",
+            containerFactory = "kafkaListenerContainerFactory"
+    )
+    public void consume(@Payload String message,
+                        @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+                        @Header(KafkaHeaders.RECEIVED_PARTITION) int partition,
+                        @Header(KafkaHeaders.OFFSET) long offset,
+                        Acknowledgment acknowledgment) {
+        try {
+            VideoUploadedModel videoUploadedModel = objectMapper.readValue(message, VideoUploadedModel.class);
+            videoMetadataServicePort.handleVideo(VideoMapper.toVideoModel(videoUploadedModel));
+
+            acknowledgment.acknowledge();
+        } catch (JsonProcessingException e) {
+            logger.log(Level.SEVERE, "Error while parsing message from topic");
+        }
     }
 }
---
+++ src/main/java/br/com/fiap_postech/video_processing_tracker/configuration/kafka/KafkaConfig.java
@@ -0,0 +1,30 @@
+package br.com.fiap_postech.video_processing_tracker.configuration.kafka;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "98.90.200.214:9094");  //"54.163.42.221:9094");//"34.224.174.155:9094");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
