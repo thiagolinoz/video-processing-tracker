dfd5aae755df6c3f138e9cb2cbefc4bfef636517
maycon-alves <maycon_valdemiro@hotmail.com>
2026-02-21 13:05:18 -0300

    @component na interface

--- src/main/java/br/com/fiap_postech/video_processing_tracker/domain/models/VideoModel.java
+++ src/main/java/br/com/fiap_postech/video_processing_tracker/domain/models/VideoModel.java
@@ -1,13 +1,12 @@
 package br.com.fiap_postech.video_processing_tracker.domain.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.Date;
 
+@Setter
+@Getter
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -24,75 +23,4 @@ public class VideoModel {
     private Date dateTimeVideoProcessCompleted = null;
     private String nmPersonName;
 
-    public String getNmPessoaEmail() {
-        return nmPessoaEmail;
-    }
-
-    public void setNmPessoaEmail(String nmPessoaEmail) {
-        this.nmPessoaEmail = nmPessoaEmail;
-    }
-
-    public String getIdVideoSend() {
-        return idVideoSend;
-    }
-
-    public void setIdVideoSend(String idVideoSend) {
-        this.idVideoSend = idVideoSend;
-    }
-
-    public String getCdVideoStatus() {
-        return cdVideoStatus;
-    }
-
-    public void setCdVideoStatus(String cdVideoStatus) {
-        this.cdVideoStatus = cdVideoStatus;
-    }
-
-    public String getNmVideo() {
-        return nmVideo;
-    }
-
-    public void setNmVideo(String nmVideo) {
-        this.nmVideo = nmVideo;
-    }
-
-    public String getNmVideoPathOrigin() {
-        return nmVideoPathOrigin;
-    }
-
-    public void setNmVideoPathOrigin(String nmVideoPathOrigin) {
-        this.nmVideoPathOrigin = nmVideoPathOrigin;
-    }
-
-    public String getNmVideoPathZip() {
-        return nmVideoPathZip;
-    }
-
-    public void setNmVideoPathZip(String nmVideoPathZip) {
-        this.nmVideoPathZip = nmVideoPathZip;
-    }
-
-    public Date getDateTimeVideoCreated() {
-        return dateTimeVideoCreated;
-    }
-
-    public void setDateTimeVideoCreated(Date dateTimeVideoCreated) {
-        this.dateTimeVideoCreated = dateTimeVideoCreated;
-    }
-
-    public Date getDateTimeVideoProcessCompleted() {
-        return dateTimeVideoProcessCompleted;
-    }
-
-    public void setDateTimeVideoProcessCompleted(Date dateTimeVideoProcessCompleted) {
-        this.dateTimeVideoProcessCompleted = dateTimeVideoProcessCompleted;
-    }
-
-    public String getNmPersonName() {
-        return nmPersonName;
-    }
-
-    public void setNmPersonName(String nmPersonName) {
-        this.nmPersonName = nmPersonName;
-    }
 }
