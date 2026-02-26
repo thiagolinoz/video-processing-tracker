2108665b05aacff472f5daca3712bb0df9ac5010
thiagolinoz <thiagolinoz@gmail.com>
2026-02-26 17:34:26 -0300

    configura o email

--- src/main/java/br/com/fiap_postech/video_processing_tracker/adapters/notification/NotificationEmail.java
+++ src/main/java/br/com/fiap_postech/video_processing_tracker/adapters/notification/NotificationEmail.java
@@ -1,6 +1,7 @@
 package br.com.fiap_postech.video_processing_tracker.adapters.notification;
 
 import br.com.fiap_postech.video_processing_tracker.domain.ports.out.NotificationPort;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Service;
 public class NotificationEmail implements NotificationPort {
 
     private final MailSender mailSender;
+    @Value("${spring.mail.username}")
+    private String emailFrom;
 
     public NotificationEmail(MailSender mailSender) {
         this.mailSender = mailSender;
@@ -17,7 +20,7 @@ public class NotificationEmail implements NotificationPort {
     @Override
     public void send(String email) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("adicionar-email-valido@email.com");
+        message.setFrom(emailFrom);
         message.setTo(email);
         message.setText("Error in processing your video, try again");
 
