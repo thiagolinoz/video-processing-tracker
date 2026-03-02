7481b78330cb5fb34d6e0a82bf35562abbf2b11a
thiagolinoz <thiagolinoz@gmail.com>
2026-03-02 20:29:27 -0300

    ajusta mecanismo envio email

--- src/main/java/br/com/fiap_postech/video_processing_tracker/configuration/mail/MailConfig.java
+++ src/main/java/br/com/fiap_postech/video_processing_tracker/configuration/mail/MailConfig.java
@@ -23,6 +23,7 @@ public class MailConfig {
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth.mechanisms", "LOGIN PLAIN");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.ssl.enable", "true");
         props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
