8d33c1393a50676e3f07ae9c8c7a5f09a8902736
thiagolinoz <thiagolinoz@gmail.com>
2026-03-02 20:11:44 -0300

    ajusta configs mail

--- src/main/java/br/com/fiap_postech/video_processing_tracker/configuration/mail/MailConfig.java
+++ src/main/java/br/com/fiap_postech/video_processing_tracker/configuration/mail/MailConfig.java
@@ -16,7 +16,7 @@ public class MailConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
         mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
+        mailSender.setPort(465);
 
         mailSender.setUsername("videopostech@gmail.com");
         mailSender.setPassword(System.getenv("SPRING_MAIL_PASSWORD"));
@@ -24,8 +24,10 @@ public class MailConfig {
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.smtp.ssl.enable", "true");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
         props.put("mail.debug", "true");
 
