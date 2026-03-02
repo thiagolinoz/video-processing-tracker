a8a643e3d4d55673747f83298950deea1243e96e
thiagolinoz <thiagolinoz@gmail.com>
2026-03-02 20:03:06 -0300

    ajusta p configurar na classe email

--- src/main/java/br/com/fiap_postech/video_processing_tracker/adapters/notification/NotificationEmail.java
+++ src/main/java/br/com/fiap_postech/video_processing_tracker/adapters/notification/NotificationEmail.java
@@ -1,30 +1,27 @@
 package br.com.fiap_postech.video_processing_tracker.adapters.notification;
 
 import br.com.fiap_postech.video_processing_tracker.domain.ports.out.NotificationPort;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationEmail implements NotificationPort {
 
-    private final MailSender mailSender;
-    @Value("${spring.mail.username}")
-    private String emailFrom;
+    private final JavaMailSender mailSender;
 
-    public NotificationEmail(MailSender mailSender) {
+    public NotificationEmail(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
 
     @Override
     public void send(String email) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(emailFrom);
+
+        message.setFrom("videopostech@gmail.com");
         message.setTo(email);
         message.setText("Error in processing your video, try again");
 
         mailSender.send(message);
-
     }
 }
